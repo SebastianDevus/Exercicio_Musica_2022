2abaa4fe0b450ebd4b964981099b9939d6139ffb
SebastianDevus <sebastianjogosextra@gmail.com>
2022-02-27 20:56:32 -0300

    Finalização da atividade, após sofrer com essas cores

---
+++ Program.cs
@@ -0,0 +1,68 @@
+﻿Console.ForegroundColor = ConsoleColor.Blue;
+Console.WriteLine(@"
+Seven Pillars of Wisdom - Sabaton"); //Título e Artista, com fundo Verde Escuro
+
+Console.ForegroundColor = ConsoleColor.White; //Volta a cor do fundo para Preto 
+Console.WriteLine(@"
+Far from home, a man with a mission
+In the heat of the glistening sun
+In the heart of ancient tradition
+This man's journey has only begun
+
+Lead the charge
+A raider has entered the battlefield
+Sabotage
+The game is about to unfold"); //Verso 1
+
+Console.ForegroundColor = ConsoleColor.Green;
+Console.WriteLine(@"
+As the darkness falls and Arabia calls
+One man spreads his wings, as the battle begins
+May the land lay claim on to Lawrence name
+Seven pillars of wisdom lights the flame"); //Refrão, com fonte verde
+
+Console.ForegroundColor = ConsoleColor.White; 
+Console.WriteLine(@"
+A revolt to gain independence
+Hide and seek, hunters hot on their trail
+Joined their ranks, obtained their acceptance
+Side by side raid the Ottoman rail
+
+Lead the charge
+Tafilah, Medina, Damascus calls
+Sabotage
+Demolish the bridges to dust"); //Verso 2
+
+Console.ForegroundColor = ConsoleColor.Green;
+Console.WriteLine(@"
+As the darkness falls and Arabia calls
+One man spreads his wings, as the battle begins
+May the land lay claim on to Lawrence name
+Seven pillars of wisdom lights the flame"); //Refrão, com fonte Verde
+
+Console.ForegroundColor = ConsoleColor.White; //Volta a cor da fonte para Branco
+Console.WriteLine(@"
+After the war has been won, deception or treason?
+Who can tell?
+Who stood to gain?
+Who stood to lose?
+Who did the dying?
+Betrayal of trust from within or compelled?
+The pillars of wisdom can tell
+Back home where a new life awaits, whispers of past
+The sands of Arabia calling"); //Ponte
+
+Console.ForegroundColor = ConsoleColor.Green;
+Console.WriteLine(@"
+As the darkness falls and Arabia calls
+One man spreads his wings, as the battle begins
+May the land lay claim on to Lawrence name
+Seven pillars of wisdom lights the flame
+
+As the darkness falls and Arabia calls
+One man spreads his wings, as the battle begins
+May the land lay claim on to Lawrence name
+Seven pillars of wisdom lights the flame
+"); //Refrão final, com fonte verde
+
+Console.ForegroundColor = ConsoleColor.White; //Volta a cor da letra para Branco
